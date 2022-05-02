8aab18c340241b19f22aa986294cb2721231b071
ADETOYESE MATTHEW <adetoyesematthew97@gmail.com>
2022-05-02 15:33:04 +0100

    wow

--- mail.php
+++ mail.php
@@ -2,13 +2,16 @@
 //get data from form
 $name = $_POST['name'],
 $email = $_POST['email'],
-$number = $_POST['mobile'],
+$subject = $_POST['subject'],
 $message = $_POST['message'];
+
 $to = "adetoyesesamson@gmail.com";
+
 $subject = "Mail from Portfolio";
-$txt = "Name = ". $name . "\r\nEmail = " . $email . "\r\nMessage =" . $message ."
-    \r\n Mobile number =". $number;
-$headers = "From: https://adetoyese607.github.io/adetoyesesamson/" . "\r\n" 
+$txt = "Name = ". $name . "\r\n Email = " . $email . "\r\n Message =" . $message ."
+    \r\n subject = ". $subject;
+
+$headers = "From: noreply@https://adetoyese607.github.io/adetoyesesamson/" . "\r\n" 
 "CC: adetoyesesamson@gmail.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
